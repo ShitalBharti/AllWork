2ca02f420bf74d53e8cfbf4abfdca2f741e54d4d
ShitalBharti <95117287+ShitalBharti@users.noreply.github.com>
2021-12-10 19:51:44 -0800

    Add files via upload
    "10-Dec-2021"  Commit

---
+++ Assignment/_01_Math_Functions.py
@@ -0,0 +1,11 @@
+import math
+
+print(round(3.9))  # round value 4
+print(abs(-3.9))  # +ve
+print(math.ceil(3.9))
+print(math.floor(3.9))
+print(3**4)
+print(bin(5))
+print(bin(10))
+print(int('0b101',2))
+
---
+++ Assignment/_03_Expression_Statements.py
@@ -0,0 +1,3 @@
+iq = 100
+user_age = iq / 5  # It is expression, bcoz it produces value
+
