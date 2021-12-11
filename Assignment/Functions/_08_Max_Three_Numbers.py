29e40d864c277be3538a43c52219cbeadfb4363b
ShitalBharti <95117287+ShitalBharti@users.noreply.github.com>
2021-12-10 20:01:31 -0800

    Delete _08_Max_Three_Numbers.py
    "Date Mismatched"

--- Assignment/Functions/_08_Max_Three_Numbers.py
+++
@@ -1,8 +0,0 @@
-def checkMax():
-    list_no = []
-    for i in range(1,4):
-        number = int(input("Enter Number {}:".format(i)))
-        list_no.append(number)
-    print("Maximum Number is {}:".format(max(list_no)))
-
-checkMax()
