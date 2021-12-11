8335d6d70b5bccb1c48af09e38c160aa546f4f92
ShitalBharti <95117287+ShitalBharti@users.noreply.github.com>
2021-12-10 20:02:33 -0800

    Delete _10_Sum_Numbers_list.py
    "Date Mismatched"

--- Assignment/Functions/_10_Sum_Numbers_list.py
+++
@@ -1,9 +0,0 @@
-def sumNumbersList():
-    range_in = int(input("Enter Range: "))
-    no_list = []
-    for i in range(1,range_in+1):
-        no = int(input('Enter no {}: '.format(i)))
-        no_list.append(no)
-    print("Sum of Numbers are:{}".format(sum(no_list)))
-
-sumNumbersList()
