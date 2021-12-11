880638cde347d9fa0210d81580f044701693406c
ShitalBharti <95117287+ShitalBharti@users.noreply.github.com>
2021-12-10 20:01:16 -0800

    Delete _07_Table.py
    "Date Mismatched"

--- Assignment/Functions/_07_Table.py
+++
@@ -1,8 +0,0 @@
-def printTable():
-    number = int(input("Enter number:"))
-    table_list = []
-    for i in range(1,11):
-        table_list.append(number * i)
-    print("Table:{}".format(table_list))
-
-printTable()
