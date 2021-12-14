0e892d83acd6177b49086ad0f492dfb7a2fdf1a6
ShitalBharti <95117287+ShitalBharti@users.noreply.github.com>
2021-12-13 19:52:02 -0800

    Delete Daily_Tasks(To_Do_App) directory
    "Date mismatched"

--- Daily_Tasks(To_Do_App)/database_Connection/models.py
+++
@@ -1,9 +0,0 @@
-from .database import Base
-from sqlalchemy import Column,Integer,String
-
-class Task(Base):
-    __tablename__ = 'tasks'
-
-    id = Column(Integer,primary_key=True,index = True)
-    task_name = Column(String)
-    task_description = Column(String)
